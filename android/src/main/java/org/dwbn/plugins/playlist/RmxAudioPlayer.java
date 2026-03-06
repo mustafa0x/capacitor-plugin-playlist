fc99fad4fbb0b069cd97aa03e6423b890ccc66f4
Mustafa J <mustafa.0x@gmail.com>
2026-03-06 21:06:51 +0300

    android: suppress selection stop events

--- android/src/main/java/org/dwbn/plugins/playlist/RmxAudioPlayer.java
+++ android/src/main/java/org/dwbn/plugins/playlist/RmxAudioPlayer.java
@@ -205,6 +205,9 @@ public class RmxAudioPlayer implements PlaybackStatusListener<AudioTrack>,
     @Override
     public void onItemPlaybackEnded(AudioTrack item) {
         if (item != null) {
+            if (suppressSelectionPlaybackEvents || isSuppressingSelection(item)) {
+                return;
+            }
             String trackId = item.getTrackId();
             JSONObject trackStatus = getPlayerStatus(item);
             onStatus(RmxAudioStatusMessage.RMXSTATUS_STOPPED, trackId, trackStatus);
@@ -254,8 +257,7 @@ public class RmxAudioPlayer implements PlaybackStatusListener<AudioTrack>,
 
         switch (playbackState) {
             case STOPPED:
-                if (suppressSelection) {
-                    clearTrackSelectionSuppression();
+                if (suppressSelection || suppressSelectionPlaybackEvents) {
                     break;
                 }
                 onStatus(RmxAudioStatusMessage.RMXSTATUS_STOPPED, "INVALID", null);
