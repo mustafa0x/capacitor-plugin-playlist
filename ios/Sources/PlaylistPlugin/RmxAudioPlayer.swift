c55a7dedb9df0fe4051a78e531f91b6324777b73
Mustafa J <mustafa.0x@gmail.com>
2026-07-26 22:09:55 +0300

    fix(ios): use a playback-only audio session

--- ios/Sources/PlaylistPlugin/RmxAudioPlayer.swift
+++ ios/Sources/PlaylistPlugin/RmxAudioPlayer.swift
@@ -1128,16 +1128,10 @@ final class RmxAudioPlayer: NSObject {
     func activateAudioSession() {
         let avSession = AVAudioSession.sharedInstance()
 
-        // If no devices are connected, play audio through the default speaker (rather than the earpiece).
-        var options: AVAudioSession.CategoryOptions = .defaultToSpeaker
-
-        // If both Bluetooth streaming options are enabled, the low quality stream is preferred; enable A2DP only.
-        options.insert(.allowBluetoothA2DP)
-
         do {
             // Always set category first, even if session is already active
             // This ensures we have the correct category after video player exits
-            try avSession.setCategory(.playAndRecord, options: options)
+            try avSession.setCategory(.playback)
         } catch {
             print("Error setting category! \(error.localizedDescription)")
         }
