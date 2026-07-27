176ed4b15d41c75dccb608f0483e0b87db659f2e
mustafa0x <mustafa.0x@gmail.com>
2026-07-27 09:28:45 +0300

    cleanup(web): rely on Capacitor plugin caching

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,15 +1,9 @@
-import {registerPlugin} from '@capacitor/core';
+import { registerPlugin } from '@capacitor/core';
 
-import type {PlaylistPlugin} from './definitions';
+import type { PlaylistPlugin } from './definitions';
 
-// todo: find out why we get imported twice
-let playListWebInstance: PlaylistPlugin;
 const Playlist = registerPlugin<PlaylistPlugin>('Playlist', {
-    web: () => import('./web').then(m => {
-        if (!playListWebInstance) {
-            playListWebInstance = new m.PlaylistWeb();
-        }
-        return playListWebInstance;
-    }),
+    web: () => import('./web').then(({ PlaylistWeb }) => new PlaylistWeb()),
 });
-export {Playlist};
+
+export { Playlist };
